a5c27cf2c0ef39c170e4a8401be0b9e85fcc2332
highjeans <highjeans11@hotmail.com>
2024-06-01 21:04:46 +0100

    Add rocket framework for potential use

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod wallet;
+
+#[macro_use] extern crate rocket;
+
+// #[launch]
+// fn start() -> _ {
+//     rocket::build().mount("/", routes![
+//
+//     ])
+// }
+
 fn main() {
-    println!("Hello, world!");
+
 }
